5bf1ae7fa222a02af040c308c908dadcd9309286
tanishqmanuja <tanishqmanuja@gmail.com>
2023-12-29 21:07:25 +0530

    fix: tty issue on server environment

--- src/utils.ts
+++ src/utils.ts
@@ -25,6 +25,10 @@ export async function downloadAPK(url: string, name: string): Promise<void> {
 }
 
 export async function waitForKeypressExit(exitCode = 0) {
+  if (!process.stdin.isTTY) {
+    return;
+  }
+
   console.log("\nPress any key to exit ...");
   process.stdin.setRawMode(true);
   return new Promise<void>((resolve) =>
